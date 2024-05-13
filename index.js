cbcbc2222a54c975c9eba5a9890b1890e07cb8d0
Julesyasaby <166526374+Julesyasaby@users.noreply.github.com>
2024-05-13 16:25:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 require('dotenv').config();
 
-async function getImage(query){
-  try{
-  const endpoint = 'https://api.giphy.com/v1/gifs/search?api_key=OUaici4OhPOQUO36XZB4Wt6SWHrBBWJ5&q=dog&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips'
-  const response = await fetch(endpoint);
-  const data = await response.json();
-  return data;
-  } 
-  catch (error){
-    console.error('There was an error fetching the image', error);
-  }
-}
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
 
-getImage('llama').then(data => console.log(data));
+async function getImage(query){
+  const response = await fetch(endpoint + "&q=" + query );
+  const data = await response.json();
+  const gifUrl = data.data[0].url
+  console.log("Gif Url:", gifUrl)
+  return gifUrl
+  } 
+
+
+getImage("cats")
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
