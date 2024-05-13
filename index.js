94dae8a5e535ccfd891b25892484352f8105992f
Julesyasaby <166526374+Julesyasaby@users.noreply.github.com>
2024-05-13 15:51:03 +0100

    Async giphy

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 require('dotenv').config();
 
+async function getImage(query){
+  try{
+  const endpoint = 'https://api.giphy.com/v1/gifs/search?api_key=OUaici4OhPOQUO36XZB4Wt6SWHrBBWJ5&q=dog&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips'
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return data;
+  } 
+  catch (error){
+    console.error('There was an error fetching the image', error);
+  }
+}
+
+getImage('llama').then(data => console.log(data));
+
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
